09ce095d156183fc6479a8303ace0421f692db5a
kelp0 <93107543+kelp0@users.noreply.github.com>
2022-06-06 15:16:10 +0700

    Add files via upload

---
+++ boris2.py
@@ -0,0 +1,12 @@
+def ebatnaxui(a, b = ''):
+    for element in a.split():
+        if len(element) >= 5:
+            c = ''.join(reversed(element))
+            b += c + ''
+        else:
+            b += element + ' '
+    return b
+            
+
+a = input()
+print(ebatnaxui(a))
